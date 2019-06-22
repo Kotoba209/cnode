15714105d0e1a83ad06cee557b21b34d1891404e
fuliang <fuliang27209@163.com>
2019-06-22 15:35:11 +0800

    cnode-bbs

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <cn-header></cn-header>
+    <router-view name='main'></router-view>
+    <router-view name='sidebar'></router-view>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import cnHeader from './components/Header'
+
+export default {
+  name: 'app',
+  components:{
+    cnHeader,
+  },
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+</script>
+
+<style>
+  html {
+    background-color: #DDDDDD;
   }
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 20px;
+  }
+  body,div,span,a,p,ul,li {
+    margin: 0;
+    padding: 0;
+  }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
+import PostList from '@/components/PostList'
+import Article from '@/components/Article'
+import SideBar from '@/components/SideBar'
+import UserInfo from '@/components/UserInfo'
 
 Vue.use(Router)
 
@@ -10,16 +14,38 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'root',
+      components: {
+        main:PostList
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path:'/topic/:id&auther=:name',
+      name:'post_content',
+      components:{
+        main:Article,
+        sidebar:SideBar
+      },
+    },
+    {
+      path:'/user/:name',
+      name:'user_info',
+      components:{
+        main:UserInfo,
+      }
     }
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: Home
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
